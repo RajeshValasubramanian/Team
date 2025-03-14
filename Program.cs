9edd18f80589903ebbeb1a8480bd764b04362dd6
RajeshValasubramanian <rajesh.valasubramanian@Relevantz.com>
2025-03-14 17:42:52 +0530

    commit from aashik

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
-
-class Tasks{
-
-    public static void Main(string[] args){
-        functions obj=new functions();
-        int a=10,b=10;
-        obj.add(a,b);//Balaji
-        obj.sub(a,b);//Aashik
-        obj.mul(a,b);//Bangaru
-        obj.div(a,b);//Sandhiya
-    }
+public void mul(int a,int b){
+    return a*b;
 }
