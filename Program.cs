49e569949501c6f89fd5b4491aa425e607c28122
RajeshValasubramanian <rajesh.valasubramanian@Relevantz.com>
2025-03-14 17:32:23 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+
+class Tasks{
+
+    public static void Main(string[] args){
+        functions obj=new functions();
+        int a=10,b=10;
+        obj.add(a,b);//Balaji
+        obj.sub(a,b);//Aashik
+        obj.mul(a,b);//Bangaru
+        obj.div(a,b);//Sandhiya
+    }
+}
